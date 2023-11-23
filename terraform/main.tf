7d173bde2e8416b4682dd5d305f14951eb393235
maran281 <81796595+maran281@users.noreply.github.com>
2023-11-23 08:45:06 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,37 +12,37 @@ terraform {
 }
 
 #Bucket which stores the souce code for the cloud function
-resource "google_storage_bucket" "cf_sourcecode_4_wf_4_tf_buk_2_pub_big" {
-  name = "cf_sourcecode_4_wf_4_tf_buk_2_pub_big"
+resource "google_storage_bucket" "cf_sc_4_wf_4_tf_buk_2_pub_big" {
+  name = "bucket_sourcecode_4_wf_4_tf_buk_2_pub_big"
   location = "us-east1"
 }
 
 #resource which places the source code object for the cloud function from local to the bucket
 resource "google_storage_bucket_object" "cf_sourcecodeobject_4_wf_4_tf_buk_2_pub_big" {
     name = "main.zip"
-    bucket = "google_storage_bucket.cf_sourcecode_4_wf_4_tf_buk_2_pub_big"  
+    bucket = "google_storage_bucket.cf_sc_4_wf_4_tf_buk_2_pub_big.name"  
     source = "../code/main.zip"
 }
 
 #Bucket which acts as a source. Cloud function from where CF picks the file and process the data
-resource "google_storage_bucket" "tf_sourcefilebucket_4_wf_4_tf_buk_2_pub_big" {
+resource "google_storage_bucket" "cf_sourcefilebucket_4_wf_4_tf_buk_2_pub_big" {
   name = "bucket_sourcefile_4_wf_4_tf_buk_2_pub_big"
   location = "us-east1"
 }
 
 #Bucket which acts as a target for CF. After processing the data CF places a file in this bucket
-resource "google_storage_bucket" "tf_targetfilebucket_4_wf_4_tf_buk_2_pub_big" {
+resource "google_storage_bucket" "cf_targetfilebucket_4_wf_4_tf_buk_2_pub_big" {
   name = "bucket_targetfile_4_wf_4_tf_buk_2_pub_big"
   location = "us-east1"
 }
 
-resource "google_pubsub_topic" "pubsub_4_wf_4_tf_buk_2_pub_big" {
+resource "google_pubsub_topic" "cf_pubsub_4_wf_4_tf_buk_2_pub_big" {
   name = "pubsub_4_wf_4_tf_buk_2_pub_big"
 }
 
 resource "google_pubsub_subscription" "pubsub_subscription_4_wf_4_tf_buk_2_pub_big" {
   name = "pubsub_subscription_4_wf_4_tf_buk_2_pub_big"
-  topic = "google_pubsub_topic.pubsub_4_wf_4_tf_buk_2_pub_big"
+  topic = "google_pubsub_topic.cf_pubsub_4_wf_4_tf_buk_2_pub_big.name"
 }
 
 resource "google_bigquery_dataset" "bq_dataset_4_wf_4_tf_buk_2_pub_big" {
@@ -56,7 +56,7 @@ resource "google_bigquery_dataset" "bq_dataset_4_wf_4_tf_buk_2_pub_big" {
     }
 }
 
-resource "google_cloudfunctions_function" "cf_4_wf_4_tf_buk_2_pub_big" {
+resource "google_cloudfunctions_function" "cf_4_wf_4_tf_buk_2_pub_big_1" {
   name = "cf_4_wf_4_tf_buk_2_pub_big"
   runtime = "python310"
   source_archive_bucket = "google_storage_bucket.cf_sourcecode_4_wf_4_tf_buk_2_pub_big.name"
@@ -68,9 +68,9 @@ resource "google_cloudfunctions_function" "cf_4_wf_4_tf_buk_2_pub_big" {
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
-  cloud_function = "google_cloudfunctions_function.cf_4_wf_4_tf_buk_2_pub_big"
+  cloud_function = "google_cloudfunctions_function.cf_4_wf_4_tf_buk_2_pub_big_1"
   member = "allUsers"
   role = "roles/cloudfunctions.invoker"
-  depends_on = [ google_cloudfunctions_function.cf_4_wf_4_tf_buk_2_pub_big ]
+  depends_on = [ google_cloudfunctions_function.cf_4_wf_4_tf_buk_2_pub_big_1 ]
 }
 
