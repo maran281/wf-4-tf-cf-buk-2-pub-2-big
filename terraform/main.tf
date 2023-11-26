64170fd57bc22503411257b0cfc6fe0d0948732c
mukesh sain <mukeshk.sain786@gmail.com>
2023-11-26 20:43:00 +0100

    commit1

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,4 +79,5 @@ resource "google_cloudfunctions_function_iam_member" "invoker" {
   member = "allUsers"
   role = "roles/cloudfunctions.invoker"
   depends_on = [ google_cloudfunctions_function.cf_4_wf_4_tf_buk_2_pub_big_1 ]
+  
 }
