6abbbc0d919759aefab77aae9fd4701fcf5ea5f6
mukesh sain <mukeshk.sain786@gmail.com>
2023-12-28 23:31:23 +0100

    poc1

--- code/main.py
+++ code/main.py
@@ -70,7 +70,7 @@ def publish_message(data, context):
         if file_status == 'success':
             bucket = storage_client.get_bucket(source_bucket)
             blob = bucket.blob(source_file_name)
-            if blob.exists:
+            if blob.exists():
                 blob.delete()
                 logger.info(f"processing of file {source_file_name} is completed and file is deleted from the source bucket")
         else:
